83454962f46e8411f21b56a85e03ef43f65e1593
simplekind <harshitgoyal394@gmail.com>
2022-04-23 16:43:29 +0530

    V0 - Side nav bar done

--- client/src/components/Sidenavbar/Sidenavbar.js
+++ client/src/components/Sidenavbar/Sidenavbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Sidenavbar.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faAngleDown,faSearch,faPlus} from '@fortawesome/free-solid-svg-icons';
+import {faAngleDown,faAngleRight,faSearch,faPlus,faStar,faNoteSticky,faBook,faPerson,faTags,faTrash} from '@fortawesome/free-solid-svg-icons';
 function Sidenavbar(){
     return (
         <div className="Sidenavbar">
@@ -25,6 +25,48 @@ function Sidenavbar(){
                     <FontAwesomeIcon className="plus-icon" icon ={faPlus}/>
                     <div className="name">
                         New Note
+                    </div>
+                </div>
+            </div>
+            <div className="sidenavbar-mid">
+                <div className="sidenavbar-mid-notes-list">
+                    <div className="sidenavbar-mid-notes-list-note">
+                        <FontAwesomeIcon className="arrow-down-icon" icon ={faAngleRight}/>
+                        <FontAwesomeIcon className="star-icon" icon ={faStar}/>
+                        <div className="name">
+                            Shortcuts
+                        </div>  
+                    </div>
+                    <div className="sidenavbar-mid-notes-list-note sidenavbar-mid-notes-list-all-notes">
+                        <FontAwesomeIcon className="note-icon" icon ={faNoteSticky}/>
+                        <div className="name">
+                            All Notes
+                        </div>  
+                    </div>
+                    <div className="sidenavbar-mid-notes-list-note sidenavbar-mid-notes-list-notebook">
+                        <FontAwesomeIcon className="arrow-down-icon" icon ={faAngleRight}/>
+                        <FontAwesomeIcon className="faBook" icon ={faBook}/>
+                        <div className="name">
+                            Notebooks
+                        </div>  
+                    </div>
+                    <div className="sidenavbar-mid-notes-list-note sidenavbar-mid-notes-list-all-notes">
+                        <FontAwesomeIcon className="note-icon" icon ={faPerson}/>
+                        <div className="name">
+                            Shared with me
+                        </div>  
+                    </div>
+                    <div className="sidenavbar-mid-notes-list-note sidenavbar-mid-notes-list-all-notes">
+                        <FontAwesomeIcon className="note-icon tag-icon" icon ={faTags}/>
+                        <div className="name">
+                            Tags
+                        </div>  
+                    </div>
+                    <div className="sidenavbar-mid-notes-list-note sidenavbar-mid-notes-list-all-notes">
+                        <FontAwesomeIcon className="note-icon tag-icon" icon ={faTrash}/>
+                        <div className="name">
+                            Trash
+                        </div>  
                     </div>
                 </div>
             </div>
