497c7d6b1551cbb8f951f4214ef9144f0d4484bd
simplekind <harshitgoyal394@gmail.com>
2022-06-02 10:28:41 +0530

    client

--- client/src/components/NoteList/NoteList.js
+++ client/src/components/NoteList/NoteList.js
@@ -3,10 +3,43 @@ import './NoteList.scss';
 
 const NoteList = ()=>{
     return(
-        <div className="NoteList">
-            <h2>
-                NoteList
-            </h2>
+        <div className="Note-List">
+            <div className="header">
+                <div className="title">
+                    <h3>All Notes</h3>
+                </div>
+                <div className="number">
+                    2 notes
+                </div>
+            </div>
+            <div className="body">
+                <div className="note-card">
+                    <div className="note-card-head">
+                        <div className="note-card-title">
+                            Note 1 :
+                        </div>
+                        <div className="note-card-desc">
+                            Bring the Cake!
+                        </div>
+                    </div>
+                    <div className="note-card-date">
+                        30 Dec 2021
+                    </div>
+                </div>
+                <div className="note-card">
+                    <div className="note-card-head">
+                        <div className="note-card-title">
+                            Note 2 :
+                        </div>
+                        <div className="note-card-desc">
+                            Hey Chek this out ! Check this out!
+                        </div>
+                    </div>
+                    <div className="note-card-date">
+                        31 Oct 2021
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- client/src/components/Notes/Notes.js
+++ client/src/components/Notes/Notes.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './Notes.scss';
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArchive} from '@fortawesome/free-solid-svg-icons';
 function Notes(){
     return(
         <div className="Notes">
-            <h2>
-                Notes
-            </h2>
+            <div className="note-header">
+                <div className="note-header-date">
+                    Last Edited on Dec 30, 2022
+                </div>
+                <div className="note-header-action-btn">
+                    <div className="action-btn">
+                        <FontAwesomeIcon icon={faArchive}/>
+                    </div>
+                </div>
+            </div>
+            <div className="note-body">
+                <div className="note-body-head">
+                    <input placeholder="Title" />
+                </div>
+                <div className="note-body-content">
+                    <textarea padding placeholder="Write your words!"/>
+                </div>
+            </div>
         </div>
     );
 }
